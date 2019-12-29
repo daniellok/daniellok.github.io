acbd13045d831d2faffc426dab9ef36e04d8895c
Daniel Lok <daniel.yk.lok@gmail.com>
2019-12-29 14:08:34 +0800

    make src a separate branch

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// @flow strict-local
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Page under construction
+      </header>
+    </div>
+  );
+}
+
+export default App;
