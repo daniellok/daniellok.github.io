3e262bf49f9a3b3d3599cf0b8094b33649abfdcc
Daniel Lok <daniel.yk.lok@gmail.com>
2019-12-29 14:27:19 +0800

    update app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 // @flow strict-local
 import React from "react";
+import { Router } from "@reach/router";
 import logo from "./logo.svg";
 import "./App.css";
 
+const Home = () => (
+  <div className="App">
+    <header className="App-header">Home page under construction</header>
+  </div>
+);
+
+const OtherPath = () => (
+  <div className="App">
+    <header className="App-header">Other page under construction too</header>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Page under construction
-      </header>
-    </div>
+    <Router>
+      <Home path="/" />
+      <OtherPath path="/other" />
+    </Router>
   );
 }
 
